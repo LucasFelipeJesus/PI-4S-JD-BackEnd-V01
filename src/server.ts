e8bbc6a15820c9f6a43ef7cf7eaa38959c19c106
LucasFelipeJesus <lucasfelipejesus@gmail.com>
2024-05-07 08:19:50 -0300

    ajuste BD

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import express from "express"
+import db1DataSource from "./database/ormconfig"
+import routes from "./routes"
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(routes)
-app.use(cors())
+//app.use(cors())
 
 app.listen(port, () => {
     console.log(`Servidor executando na porta ${port}`);
 
-    //console.log(database.isInitialized ? 'Banco Sqllite Dados OK! :' : 'Banco Sqllite Carregando!')
+    //console.log(database.isInitialized ? 'Banco Sqllite Dados OK! :' : 'Banco Sqllite Carregando!')   
 
-    //console.log(db2DataSource.isInitialized ? 'Banco Mysql Dados OK! :' : 'Banco Mysql Carregando!')
-
-    console.log(db3DataSource.isInitialized ? 'Banco Sqlserver Dados OK! :' : 'Banco Sqlserver Carregando!')
-})c
+    console.log(db1DataSource.isInitialized ? 'Banco Sqlserver Dados OK! :' : 'Banco Sqlserver Carregando!')
+})
